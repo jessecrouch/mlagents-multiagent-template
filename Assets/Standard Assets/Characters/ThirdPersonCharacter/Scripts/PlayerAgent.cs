99994b39097a8251331a27b94cf5c5fcee431c5e
Jesse Crouch <jesse@jessecrouch.com>
2020-11-11 11:48:54 -0600

    Accepts basic horz/vert inputs via Heuristic->OnActionReceived->ThirdPersonUserControl.cs!

---
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/PlayerAgent.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+using UnityStandardAssets.CrossPlatformInput;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+public class PlayerAgent : Agent
+{
+    private ThirdPersonUserControl userControl;
+
+    public override void Initialize()
+    {
+        userControl = GetComponentInChildren<ThirdPersonUserControl>();
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        // sensor.AddObservation();
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        userControl.verticalInput = vectorAction[0];
+        userControl.horizontalInput = vectorAction[1];
+    }
+
+    public override void OnEpisodeBegin()
+    {
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        actionsOut[0] = CrossPlatformInputManager.GetAxis("Vertical"); // forward
+        actionsOut[1] = CrossPlatformInputManager.GetAxis("Horizontal"); // sideways
+        // actionsOut[2] = Input.GetKey(KeyCode.C); // crouch
+    }
+}
---
+++ Assets/Standard Assets/Characters/ThirdPersonCharacter/Scripts/TemplateAgent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.MLAgents;
+using Unity.MLAgents.Sensors;
+
+public class TemplateAgent : Agent
+{
+    public override void Initialize()
+    {
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+    }
+
+    public override void OnEpisodeBegin()
+    {
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+    }
+}
